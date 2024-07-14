b53ecdf83d4cfa9afdfa5ade615060f963c799ea
seblful <alesha.chimba@gmail.com>
2024-07-14 18:00:29 +0300

    [EDIT] quiz handler

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from modules.quiz import OpentdbAPIHandler
+import os
+
+from modules.quiz import OpentdbAPIHandler, QuizHandler
+
+HOME = os.getcwd()
+DATA_DIR = os.path.join(HOME, 'data')
 
 
 def main() -> None:
-    api_handler = OpentdbAPIHandler()
+    api_handler = OpentdbAPIHandler(json_dir=DATA_DIR)
+    quiz_handler = QuizHandler(json_dir=DATA_DIR)
 
     api_handler.download_questions(q_type="multiple")
 
