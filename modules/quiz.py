f59143a255c6566113dbb7bfbc54ca86a2123517
seblful <alesha.chimba@gmail.com>
2024-07-14 18:00:03 +0300

    [EDIT] response token

--- modules/quiz.py
+++ modules/quiz.py
@@ -1,17 +1,26 @@
-import requests
+import os
 import json
+import requests
 import time
+import random
 
 from typing import List
 
 
 class OpentdbAPIHandler:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 json_dir: str) -> None:
+        self.json_dir: str = json_dir
 
     def download_questions(self, q_type: str, amount: int = 50) -> None:
+        # Get token
+        response_token = requests.get(
+            "https://opentdb.com/api_token.php?command=request")
+        session_token = response_token.json()["token"]
+
         # API endpoint
-        url = f"https://opentdb.com/api.php?amount={amount}&type={q_type}"
+        url = f"https://opentdb.com/api.php?amount={
+            amount}&type={q_type}&token={session_token}"
 
         # Initialize an empty list to store the questions
         questions: List[int] = []
@@ -30,16 +39,11 @@ class OpentdbAPIHandler:
                 if data['response_code'] == 0:
                     # Add the questions to the list
                     questions.extend(data['results'])
-
                     print(f"It was downloaded {len(questions)} questions.")
 
                     # Check if there are more questions to retrieve
                     if len(data['results']) < amount:
                         break
-
-                    # Update the URL to retrieve the next batch of questions
-                    url = f"https://opentdb.com/api.php?amount={amount}&type={q_type}&page={
-                        len(questions)//amount}"
 
                     # Delay for 5 seconds to avoid exceeding the rate limit
                     time.sleep(5)
@@ -47,8 +51,8 @@ class OpentdbAPIHandler:
                     print(f"Error: {data['response_message']}")
                     break
             elif response.status_code == 429:
-                print("Rate limit exceeded. Waiting for 60 seconds...")
-                time.sleep(60)
+                print("Rate limit exceeded. Waiting for 10 seconds...")
+                time.sleep(10)
             else:
                 print(f"Error: {response.status_code}")
                 break
@@ -57,15 +61,20 @@ class OpentdbAPIHandler:
         self.save_json(q_type=q_type,
                        questions=questions)
 
+    def save_json(self, q_type: str,  questions: str) -> None:
+        # Save the questions to a JSON file
+        json_path: str = os.path.join(
+            self.json_dir, f"data/trivia_questions_{q_type}.json")
+        with open(json_path, "w") as file:
+            json.dump(questions, file, indent=2)
 
-def save_json(self, q_type: str,  questions: str) -> None:
-    # Save the questions to a JSON file
-    with open(f"trivia_questions_{q_type}.json", "w") as file:
-        json.dump(questions, file, indent=2)
-
-    print(f"Questions with {q_type} type was saved.")
+        print(f"Questions with {q_type} type was saved.")
 
 
 class QuizHandler:
-    def __init__():
+    def __init__(self,
+                 json_dir: str) -> None:
+        self.json_dir = json_dir
+
+    def get_random_question(self):
         pass
