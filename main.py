fa3b6c6c3b48956f4b843ce9a2b1930f48beab89
seblful <alesha.chimba@gmail.com>
2024-07-20 20:39:13 +0300

    [EDIT] run game

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import os
+import time
 
 from modules.quiz import OpentdbAPIHandler, QuizHandler
+from modules.game import GameCreator
 
 HOME = os.getcwd()
-DATA_DIR = os.path.join(HOME, 'data')
+JSON_DIR = os.path.join(HOME, 'data')
+SOURCE_DIR = os.path.join(HOME, 'source')
 
 
 def main() -> None:
     # # Download questions
-    # api_handler = OpentdbAPIHandler(json_dir=DATA_DIR)
+    # api_handler = OpentdbAPIHandler(json_dir=JSON_DIR)
     # api_handler.download_questions(q_type="multiple")
+    # time.sleep(60)
     # api_handler.download_questions(q_type="boolean")
 
     # Get question
-    quiz_handler = QuizHandler(json_dir=DATA_DIR)
+    quiz_handler = QuizHandler(json_dir=JSON_DIR)
     question = quiz_handler.get_random_question(q_type="multiple")
     print(question)
+
+    # Run game
+    game_creator = GameCreator(json_dir=JSON_DIR,
+                               source_dir=SOURCE_DIR)
+    game_creator.run()
 
 
 if __name__ == "__main__":
