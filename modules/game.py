f5e7579f6adf5da92162338d65bacf71644ef446
seblful <alesha.chimba@gmail.com>
2024-07-20 15:05:40 +0300

    [EDIT] new init

--- modules/game.py
+++ modules/game.py
@@ -1,23 +1,25 @@
-from video import VideoCreator
+from typing import Tuple
+
+from quiz import QuizHandler
 
 
 class GameCreator:
     def __init__(self,
-                 topic_duration: int = 120,
-                 question_duration: int = 300,
-                 answer_duration: int = 60) -> None:
+                 json_dir: str,
+                 frame_size: Tuple[int, int] = (1080, 1920),
+                 fps: int = 60) -> None:
 
         # Game mods
         self.game_mods = ("start", "topic", "question", "answer")
+        self.mode_durs = (60, 120, 300, 60)  # Duration  of game mods
         self.mode_idx = 0
 
-        # Duration of mods
-        self.topic_duration = topic_duration,
-        self.question_duration = question_duration,
-        self.answer_duration = answer_duration
+        # Questions
+        self.quiz_handler = QuizHandler(json_dir=json_dir)
 
         # Video
-        video_creator = VideoCreator
+        self.frame_size = frame_size
+        self.fps = fps
 
     def run(self):
         pass
