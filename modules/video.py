d3d1ba05395eaddb2daf23c4b03663a5eef07216
seblful <alesha.chimba@gmail.com>
2024-07-20 14:15:39 +0300

    [REN] rename graphic to video

---
+++ modules/video.py
@@ -0,0 +1,11 @@
+from typing import Tuple
+
+
+class VideoCreator:
+    def __init__(self,
+                 frame_size: Tuple[int, int] = (1080, 1920),
+                 ):
+        self.frame_size = frame_size
+
+    def render(self):
+        pass
